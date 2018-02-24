039b1b8e954c9c57b5b6b1a9585ea2b60c88c6a4
胡国伟 <ihuguowei@gmail.com>
2018-02-24 12:08:48 +0800

    feat(‘custom functions’): add some custom functions

--- functions.php
+++ functions.php
@@ -7,71 +7,93 @@
  */
 
 
-if (!function_exists('utf8Substr')) {
-    function utf8Substr($str, $from, $len) {
-        return preg_replace('#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$from.'}'.
-            '((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$len.'}).*#s',
-            '$1',$str);
-    }
+if ( ! function_exists( 'utf8Substr' ) ) {
+	function utf8Substr( $str, $from, $len ) {
+		return preg_replace( '#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,' . $from . '}' .
+		                     '((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,' . $len . '}).*#s',
+			'$1', $str );
+	}
 }
 
-// 自定义 header
-add_theme_support( 'custom-header' ,array(
-	'default-image'          => get_template_directory_uri() . '/images/header_default.png',
-	'width'                  => 1000,
-	'height'                 => 500,
-	'flex-height'            => true,
-	'flex-width'             => true,
-	'uploads'                => true,
-	'random-default'         => true,
-	'header-text'            => true,
-	'default-text-color'     => '#333',
-	'wp-head-callback'       => '',
-	'admin-head-callback'    => '',
-	'admin-preview-callback' => '',
-));
+
+// Register Theme Features
+function custom_theme_features() {
+
+	// Add theme support for Automatic Feed Links
+	add_theme_support( 'automatic-feed-links' );
+
+	// 自定义 header
+	add_theme_support( 'custom-header', array(
+		'default-image'          => get_template_directory_uri() . '/images/header_default.png',
+		// Display the header text along with the image
+		'header-text'            => false,
+		// Header text color default
+		'default-text-color'     => '000',
+		// Header image width (in pixels)
+		'width'                  => 1000,
+		// Header image height (in pixels)
+		'height'                 => 500,
+		// Header image random rotation default
+		'random-default'         => false,
+		// Enable upload of image file in admin
+		'uploads'                => true,
+		// function to be called in theme head section
+		'wp-head-callback'       => 'wphead_cb',
+		//  function to be called in preview page head section
+		'admin-head-callback'    => 'adminhead_cb',
+		// function to produce preview markup in the admin screen
+		'admin-preview-callback' => 'adminpreview_cb',
+	) );
 
 
-// 自定义 logo
-add_theme_support('custom-logo', array(
-	'height'      => 100,
-	'width'       => 400,
-	'flex-height' => true,
-	'flex-width'  => true,
-	'header-text' => array( 'site-title', 'site-description' ),
-));
+	// 自定义 logo
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-height' => true,
+		'flex-width'  => true,
+		'header-text' => array( 'site-title', 'site-description' ),
+	) );
 
-// 自定义背景图片
-add_theme_support( 'custom-background', array(
-	'default-color'          => '#FFF',
-	'default-image'          => '',
-	'default-repeat'         => '',
-	'default-position-x'     => '',
-	'default-attachment'     => '',
-	'wp-head-callback'       => '_custom_background_cb',
-	'admin-head-callback'    => '',
-	'admin-preview-callback' => ''
-));
+	// 自定义背景图片
+	add_theme_support( 'custom-background', array(
+		'default-color'          => '#FFF',
+		'default-image'          => '',
+		'default-repeat'         => 'no-repeat',
+		'default-position-x'     => 'left',
+		'default-position-y'     => 'top',
+		'default-size'           => 'auto',
+		'default-attachment'     => 'fixed',
+		'wp-head-callback'       => '_custom_background_cb',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => ''
+	) );
 
-// 自定义文章类型
-add_theme_support('post-formats', array(
-	'aside',
-	'gallery',
-	'link',
-	'image',
-	'quote',
-	'status',
-	'video',
-	'audio',
-	'chart'
-));
+	// 自定义文章类型
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'gallery',
+		'link',
+		'image',
+		'quote',
+		'status',
+		'video',
+		'audio',
+		'chart'
+	) );
+
+	// 支持 html 5
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption'
+	) );
+
+	add_theme_support( 'post-thumbnails', array() );
+}
 
 
-add_theme_support( 'html5', array(
-	'search-form',
-	'comment-form',
-	'comment-list',
-	'gallery',
-	'caption'
-));
+add_action( 'after_setup_theme', 'custom_theme_features' );
 
--- index.php
+++ index.php
@@ -2,198 +2,280 @@
 <?php
 // TODO: for better SEO!
 // blog info
-$blog_title = get_bloginfo('title') . '-' . get_bloginfo('description');
+$blog_title = get_bloginfo( 'title' ) . '-' . get_bloginfo( 'description' );
 // description
-$blog_description = get_bloginfo('description');
+$blog_description = get_bloginfo( 'description' );
 // keywords
 $blog_keywords = "";
 // author ID
 $post_author_id = $post->post_author;
 // blog author
-$blog_author = get_bloginfo('admin_email');
+$blog_author = get_bloginfo( 'admin_email' );
 
 // ID
 $post_id = get_the_ID();
-if (is_home()) {
+if ( is_home() ) {
 
-} else if (is_single()) {
-    $blog_title = single_post_title('', false);
+} else if ( is_single() ) {
+	$blog_title = single_post_title( '', false );
 
-    if (get_the_excerpt()) { // 默认读取文章的摘要信息
-        $blog_description = get_the_excerpt($post_id);
-    } else {
-        $blog_description = strip_tags($post->post_content);
-    }
-    $blog_description = preg_replace('/\s+/', ' ', $blog_description);
-    $blog_description = mb_strimwidth($blog_description, 0, 200);
+	if ( get_the_excerpt() ) { // 默认读取文章的摘要信息
+		$blog_description = get_the_excerpt( $post_id );
+	} else {
+		$blog_description = strip_tags( $post->post_content );
+	}
+	$blog_description = preg_replace( '/\s+/', ' ', $blog_description );
+	$blog_description = mb_strimwidth( $blog_description, 0, 200 );
 
-    // join all the tags
-    $blog_keywords = array();
-    foreach (wp_get_post_tags($post_id) as $tag) {
-        array_push($blog_keywords, $tag->name);
-    }
-    $blog_keywords = join($blog_keywords, ',');
+	// join all the tags
+	$blog_keywords = array();
+	foreach ( wp_get_post_tags( $post_id ) as $tag ) {
+		array_push( $blog_keywords, $tag->name );
+	}
+	$blog_keywords = join( $blog_keywords, ',' );
 
-    $blog_author = get_the_author_meta('nicename',$post_author_id);
-    if ( get_the_author_meta('email', $post_author_id) ) {
-        $blog_author = $blog_author . ',' . get_the_author_meta('email',$post_author_id);
-    }
-} else if (is_archive()) {
-    echo "is archive";
-} else if (is_search()) {
-
-} else if (is_tag()) {
+	$blog_author = get_the_author_meta( 'nicename', $post_author_id );
+	if ( get_the_author_meta( 'email', $post_author_id ) ) {
+		$blog_author = $blog_author . ',' . get_the_author_meta( 'email', $post_author_id );
+	}
+} else if ( is_archive() ) {
+	echo "is archive";
+} else if ( is_search() ) {
+	echo "is search";
+} else if ( is_tag() ) {
+	echo "is tag";
 }
 ?>
-<html lang="<?php echo get_bloginfo('language'); ?>">
-    <head>
-        <title><?php echo trim($blog_title);?></title>
-        <meta charset="<?php echo get_bloginfo('charset');?>">
-        <meta name="keywords" content="<?php echo trim($blog_keywords); ?>">
-        <meta name="description" content="<?php echo trim($blog_description); ?>">
-        <meta name="author" content="<?php echo trim($blog_author); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="renderer" content="webkit"/>
-        <meta name="force-rendering" content="webkit"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-        <?php
-        wp_register_style(
-            'pure-main',
-            get_template_directory_uri() . '/styles/index.css',
-            array(),
-            0.1,
-            'all'
-        );
-        wp_enqueue_style('pure-main');
-        wp_print_styles();
-        ?>
-    </head>
-    <body class="<?php echo body_class(); ?>">
-        <header class="main-header">
-            <nav class="header-top-bar">
-                <ul class="list">
-                    <li class="list-item"><a href="">Facebook</a></li>
-                </ul>
-            </nav>
-            <div class="main-header-content">
+<html lang="<?php echo get_bloginfo( 'language' ); ?>">
+<head>
+    <title><?php echo trim( $blog_title ); ?></title>
+    <meta charset="<?php echo get_bloginfo( 'charset' ); ?>">
+    <meta name="keywords" content="<?php echo trim( $blog_keywords ); ?>">
+    <meta name="description" content="<?php echo trim( $blog_description ); ?>">
+    <meta name="author" content="<?php echo trim( $blog_author ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit"/>
+    <meta name="force-rendering" content="webkit"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<?php
+	wp_register_style(
+		'pure-main',
+		get_template_directory_uri() . '/scss/index.min.css',
+		array(),
+		0.1,
+		'all'
+	);
 
-                <h1 class="blog-title"><img height="60px" style="vertical-align:middle;margin-right: 10px" src="https://s.w.org/about/images/logos/wordpress-logo-notext-rgb.png"><a class="home-link" href="<?php echo bloginfo("url");?>"><?php echo bloginfo("title")?></a></h1>
-                <h2 class="blog-description"><?php echo bloginfo("description")?></h2>
-                <?php
-                wp_nav_menu( array(
-                    'theme_location'  => 'header_menu',
-                    'menu'            => 'header-menu',
-                    'menu_class'      => 'top-menu',
-                    'menu_id'         => 'topMenu',
-                    'container'       => 'nav',
-                    'container_class' => 'top-nav-container',
-                    'container_id'    => 'topNavigationContainer',
-                    'echo'            => true,
-                    'fallback_cb'     => 'wp_page_menu',
-                    'before'          => '',
-                    'after'           => '',
-                    'link_before'     => '',
-                    'link_after'      => '',
-                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                    'depth'           => 1,
-                    'walker'          => ''
-                ));
-                ?>
-            </div>
-        </header>
+	wp_register_script(
+		'pure-prism-js',
+		get_template_directory_uri() . '/scripts/prism.js',
+		array(),
+		'1.11.0'
+	);
 
-        <div class="post-list">
-            <?php if ( have_posts() ) {
-                while ( have_posts() ) : the_post();
-                    ?>
-                    <article <?php post_class('post');?> id="article-<?php get_the_ID();?>">
-                        <div class="post-wrap">
-                            <h1 class="post-title">
-                                <a class="post-title-url"
-                                   href="<?php the_permalink();?>"
-                                   title="<?php the_title(); ?>"
-                                >
-                                    <?php the_title(); ?>
-                                </a>
-                            </h1>
-                            <div>
-                                <ul class="post-meta">
-                                    <li class="post-meta-item">
-                                        By:<a href="<?php echo get_the_author_meta('url')?>"
-                                              class=""
-                                              title="<?php echo get_the_author_meta('nicename');?>"><?php echo get_the_author_meta('nicename');?></a>
-                                    </li>
-                                    <li class="post-meta-item">
-                                        @<time class="publish-time"
-                                               datetime="<?php echo get_post_time('Y/m/d');?>">
-                                            <?php echo get_post_time('Y/m/d');?>
-                                        </time>
-                                    </li>
-                                    <?php if (get_the_category_list()) { ?>
-                                    <li class="post-meta-item">
-                                        In:<?php the_category('/');?>
-                                    </li>
-                                    <?php }?>
-                                </ul>
-                            </div>
-                            <div class="post-entry">
-                                <?php the_content("Read More"); ?>
-                            </div>
-                            <div class="post-meta">
-                                <?php the_tags('',','); ?>
-                            </div>
-                        </div>
-                    </article>
+	wp_enqueue_style( 'pure-main' );
+
+	wp_enqueue_script( 'pure-prism-js' );
+	wp_print_styles();
+	wp_print_scripts();
+	?>
+
+	<?php wp_head(); ?>
+</head>
 
 
-                    <?php
-                endwhile;
-            }
-            else {
-                _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-            }
-            ?>
+<body class="<?php body_class(); ?>">
+<header class="main-header">
+
+    <img class="custom-header-background-image"
+         src="<?php header_image(); ?>"
+         height="<?php echo get_custom_header()->height; ?>"
+         width="<?php echo get_custom_header()->width; ?>"
+    />
+
+    <nav class="header-top-bar">
+        <ul class="list">
+            <li class="list-item"><a href="">Facebook</a></li>
+        </ul>
+    </nav>
 
 
-            <?php if (is_page()) { ?>
+    <div class="main-header-content">
+        <div class="title-wrap">
+            <img height="60px"
+                 style="vertical-align:middle;margin-right: 10px"
+                 src="">
+            <h1 class="blog-title">
+                <a class="home-link"
+                   href="<?php bloginfo( "url" ); ?>">
+					<?php bloginfo( "title" ) ?>
+                </a>
+            </h1>
 
-                <?php ;?>
-
-            <?php } ?>
-
-
-            <?php
-            if (is_single()) {
-                if (get_previous_post_link()) { ?>
-                    <?php echo get_previous_post_link('%link');?>
-            <?php }
-                if (get_next_post_link()) {
-
-            ?>
-                    <?php echo get_next_post_link('%link'); ?>
-            <?php
-                }
-            }
-            ?>
-
-
-            <nav class="post-nav" id="postNav">
-                <?php echo paginate_links( array(
-                    'type' => 'list'
-                ) ); ?>
-            </nav>
-
-
-
+            <h2 class="blog-description">
+				<?php bloginfo( "description" ) ?>
+            </h2>
         </div>
 
-        <footer class="main-footer" id="main-footer">
-            <div class="footer-content">
-                Powered by WordPress
-                Theme By Hubery
+		<?php
+		wp_nav_menu( array(
+			'theme_location'  => 'header_menu',
+			'menu'            => 'header-menu',
+			'menu_class'      => 'top-menu',
+			'menu_id'         => 'topMenu',
+			'container'       => 'nav',
+			'container_class' => 'top-nav-container',
+			'container_id'    => 'topNavigationContainer',
+			'echo'            => true,
+			'fallback_cb'     => 'wp_page_menu',
+			'before'          => '',
+			'after'           => '',
+			'link_before'     => '',
+			'link_after'      => '',
+			'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			'depth'           => 1,
+			'walker'          => ''
+		) );
+		?>
+    </div>
+</header>
+
+<div class="post-list">
+	<?php if ( have_posts() ) {
+		while ( have_posts() ) : the_post();
+			?>
+            <div <?php post_class( 'post' ); ?>>
+                <article class="post-wrap"
+                         id="article-<?php get_the_ID(); ?>">
+                    <h1 class="post-title serif">
+                        <a class="post-title-url"
+                           href="<?php the_permalink(); ?>"
+                           title="<?php the_title(); ?>"
+                        >
+							<?php the_title(); ?>
+                        </a>
+                    </h1>
+                    <div class="posy-meta-wrap">
+                        <ul class="post-meta post-meta-top">
+                            <li class="post-meta-item author-avatar-wrap">
+                                <img class="author-avatar"
+                                     src="<?php echo get_avatar_url( get_the_author_meta( 'ID' ) ); ?>"
+                                     alt="<?php echo get_the_author_meta( 'nicename' ); ?>">
+                            </li>
+                            <li class="post-meta-item">
+                                <a class="author-name"
+                                   href="<?php echo get_the_author_meta( 'url' ) ?>"
+                                   title="<?php echo get_the_author_meta( 'display_name' ); ?>">
+									<?php echo get_the_author_meta( 'display_name' ); ?>
+                                </a>
+                                <time class="publish-time"
+                                      datetime="<?php echo get_post_time( 'Y/m/d' ); ?>">
+									<?php echo get_post_time( 'Y/m/d' ); ?>
+                                </time>
+                            </li>
+
+                            <li class="post-meta-item category-list">
+								<?php if ( get_the_category_list() ) { ?>
+									<?php the_category( '/' ); ?>
+								<?php } ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="post-entry typo serif">
+						<?php echo get_the_content( "" ); ?>
+                    </div>
+                    <div class="post-meta post-tags-wrap">
+						<?php the_tags( '', '', '' ); ?>
+                    </div>
+
+
+					<?php
+					if ( is_single() ) { ?>
+                        <nav>
+							<?php
+							if ( get_previous_post_link() ) { ?>
+								<?php echo get_previous_post_link( '%link' ); ?>
+							<?php }
+							if ( get_next_post_link() ) {
+
+								?>
+
+								<?php echo get_next_post_link( '%link' ); ?>
+								<?php
+							}; ?>
+                        </nav>
+						<?php
+					}
+					?>
+
+
+                    <div>
+		                <?php
+		                if ( comments_open() || get_comments_number() ) {
+			                comments_template();
+		                }
+
+		                ;?>
+                    </div>
+                </article>
+
+
+
             </div>
 
-        </footer>
-    </body>
 
+		<?php
+		endwhile;
+	} else {
+		_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+	}
+	?>
+
+
+	<?php if ( is_page() ) { ?>
+
+		<?php ; ?>
+
+	<?php } ?>
+
+
+
+
+
+
+    <nav class="post-nav" id="postNav">
+		<?php echo paginate_links( array(
+			'type'               => 'list',
+			'base'               => '%_%',
+			'format'             => '?paged=%#%',
+			'show_all'           => false,
+			'end_size'           => 1,
+			'mid_size'           => 2,
+			'prev_next'          => true,
+			'prev_text'          => __( '« Previous' ),
+			'next_text'          => __( 'Next »' ),
+			'add_args'           => false,
+			'add_fragment'       => '',
+			'before_page_number' => '',
+			'after_page_number'  => ''
+		) ); ?>
+    </nav>
+
+</div>
+
+<footer class="main-footer" id="main-footer">
+    <div class="footer-content">
+        Powered by <a href="https://wordpress.org/" title="code is poetry">WordPress</a><br>
+        Theme By <?php ; ?>
+    </div>
+</footer>
+
+
+<?php wp_footer(); ?>
+
+<script type="text/javascript">
+    Prism.highlightAll()
+</script>
+
+</body>
 </html>
