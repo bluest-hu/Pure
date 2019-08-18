b51054348b4ba5cc59addf82a7737ca1264bceb9
深蓝 <ihuguowei@gmail.com>
2019-08-18 13:19:03 +0800

    feat(取消 AMP 模式下缩略图展示)

--- inc/sw.php
+++ inc/sw.php
@@ -1,60 +1,60 @@
-const CACHE_NAME = '24d20f' + '_v_<?php echo get_option("pure_theme_pwa_cache_version"); ?>';
+const CACHE_NAME = '55c29a' + '_v_<?php echo get_option("pure_theme_pwa_cache_version"); ?>';
 
 console.log(CACHE_NAME);
 let initCacheResourceList = [
 ];
 
 self.addEventListener('install', (event) => {
-    caches.open(initCacheResourceList).then(cache => {
-        console.log(cache);
-    });
-    event.waitUntil(
-        // self.skipWaiting()
-        Promise.all([
-            caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(initCacheResourceList)),
-            caches.keys()
-            .then(cacheList => {
-                cacheList.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        caches.delete(cacheName);
-                    }
-                });
-            })
-        ])
-    );
+  caches.open(initCacheResourceList).then(cache => {
+    console.log(cache);
+  });
+  event.waitUntil(
+    // self.skipWaiting()
+    Promise.all([
+      caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(initCacheResourceList)),
+      caches.keys()
+        .then(cacheList => {
+          cacheList.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              caches.delete(cacheName);
+            }
+          });
+        })
+    ])
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response =>  {
-            console.log('match', response);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        console.log('match', response);
 
-            if (response) {
-                return response ;
+        if (response) {
+          return response;
+        }
+        let fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
             }
-            let fetchRequest = event.request.clone();
 
-            return fetch(fetchRequest)
-            .then(response => {
-                if (!response || response.status !== 200 || response.type !== 'basic') {
-                    return response;
-                }
+            let responseCache = response.clone();
 
-                let responseCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseCache);
+              });
 
-                caches.open(CACHE_NAME)
-                .then(cache => {
-                    cache.put(event.request, responseCache);
-                });
-
-                return response;
-            });
-        })
-    );
+            return response;
+          });
+      })
+  );
 });
 
 self.addEventListener('active', (event) => {
-    console.log('i am ', event);
+  console.log('i am ', event);
 });
