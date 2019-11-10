aebd416aafcb56fc1f52fa09e15ac9cb1fddd737
深蓝 <ihuguowei@gmail.com>
2019-11-10 21:48:14 +0800

    feat:完善归档页面
    close #1

--- archieve.php
+++ archieve.php
@@ -15,40 +15,74 @@
         <h1 class="post-title entry-title"><?php echo esc_html(get_the_title()); ?></h1>
       </header>
 
-      <div class="post-entry entry-content typo">
+      <div class="post-entry entry-content typo archive">
         <?php
-        $args = array(
-          'post_type' => 'post', //如果你有多个 post type，可以这样 array('post', 'product', 'news')
-          'posts_per_page' => -1, //全部 posts
-          'ignore_sticky_posts' => true, //忽略 sticky posts
-          'cache_results' => true,
-        );
-        $the_query = new WP_Query( $args );
+        const PURE_THEME_CACHE_FLAG = 'KEY_theme_pure_archive';
+        $result = wp_cache_get(PURE_THEME_CACHE_FLAG);
 
-        $result = array();
-
-        while ( $the_query->have_posts() ) {
-          $the_query->the_post();
-          $post_year = get_the_time('Y');
-          $post_mon = get_the_time('m');
-          $post_day = get_the_time('d');
-
-          $result[$post_year][$post_mon] = array(
-            'title' => get_the_title(),
-            'publish_date' => get_the_time('d日: '),
+        if (!$result) {
+          $result = array();
+          $args = array(
+            'post_type' => 'post', //如果你有多个 post type，可以这样 array('post', 'product', 'news')
+            'posts_per_page' => -1, //全部 posts
+            'ignore_sticky_posts' => true, //忽略 sticky posts
+            'cache_results' => true,
           );
+          $the_query = new WP_Query($args);
+          
+          while ($the_query->have_posts()) {
+            $the_query->the_post();
+            $post_year = get_the_time('Y');
+            $post_mon = get_the_time('m');
+            $post_day = get_the_time('d');
+  
+            $result[$post_year][$post_mon][] = array(
+              'title' => get_the_title(),
+              'publish_date' => get_post_time('d'),
+              'post_status' => 'publish',
+              'permalink' => get_the_permalink(),
+              'comments' => get_comments_number('0', '1', '%'),
+            );
+          }
+          wp_reset_postdata();
+          wp_cache_set(PURE_THEME_MANIFEST_KEY, $result);
         }
 
-        echo '<pre>';
-        var_dump($result);
-        echo '</pre>';
-
-        wp_reset_postdata();
+        add_action('save_post', function () {
+          wp_cache_delete(PURE_THEME_CACHE_FLAG);
+        });
         ?>
+        <ol class="year-list">
+          <?php foreach ($result as $year => $year_value) : ?>
+            <li class="year-list-item">
+              <h2 class="year-title"><?php echo $year; ?><span class="unit">年</span></h2>
+              <ol class="month-list">
+                <?php foreach ($year_value as $month => $month_value) : ?>
+                  <li class="month-list-item">
+                    <h3 class="month-title"><?php echo $month; ?><span class="unit">月</span></h2>
+                    <ol class="post-list">
+                      <?php foreach ($month_value as $post => $post_value) : ?>
+                        <li class="post-list-item">
+                          <time class="publish-time"><?php echo $post_value['publish_date']; ?>日</time>
+                          <a class="title" title="<?php echo $post_value['permalink']; ?>" href="<?php echo $post_value['permalink']; ?>"><?php echo $post_value['title'];?></a>
+                          <span class="line"></span>
+                          <a class="comments" href="<?php echo $post_value['permalink'];?>#comments"><?php echo $post_value['comments']; ?> reply</a>
+                        </li>
+                      <?php endforeach ?>
+                    </ol>
+                  </li>
+                <?php endforeach ?>
+              </ol>
+            </li>
+          <?php endforeach ?>
+        </ol>
+
+        <div class="end">
+           <span class="eof">EOF</span> 
+        </div> 
       </div>
     </article>
   </div>
 </div>
-
 
 <?php get_footer(); ?>
